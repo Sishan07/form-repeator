26a3edc7395df437957d142cfd4b4e382e38f263
Sishan07 <134852717+Sishan07@users.noreply.github.com>
2023-07-19 09:28:25 +0545

    Add files via upload

---
+++ custom/footer.php
@@ -0,0 +1,19 @@
+<footer class="site-footer">
+    <p><?php bloginfo('name'); ?> - &copy; <?php echo date('y'); ?></p>
+    <nav class="site-nav">
+    <?php
+                    $args = array(
+                        'theme_location' => 'footer'
+                    );
+                ?>
+        <?php wp_nav_menu($args); ?>
+    </nav>
+
+</footer>
+
+</div>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
